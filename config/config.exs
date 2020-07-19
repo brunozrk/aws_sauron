964afc1e6d27d52738770619acc691eba40181d6
Bruno Zeraik <brunozrk@gmail.com>
2020-07-19 10:57:53 -0300

    Adding phoenix

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "qhhUB4GPSaNRmUABio/aM9tdddIZkVpv4XyPyp0Wj+VI3Q/l4fF7yfJUCyifbmEl",
+  render_errors: [view: Web.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Web.PubSub,
+  live_view: [signing_salt: "OSHIxSWO"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -22,3 +33,15 @@ config :ex_aws,
   # secret_access_key: {:system, "AWS_SECRET_ACCESS_KEY"}
   region: "sa-east-1",
   json_codec: Jason
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
