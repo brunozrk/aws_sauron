4cc6ebd33be170fa9a08e7feb3c65ac09e974cda
Bruno Zeraik <brunozrk@gmail.com>
2020-07-19 19:09:53 -0300

    add server app as dependencie of web app

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -45,7 +45,8 @@ defmodule Web.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:server, in_umbrella: true}
     ]
   end
 
