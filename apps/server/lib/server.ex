399c5795acc34bf422409d077ead9d7474fb2204
Bruno Zeraik <brunozrk@gmail.com>
2020-07-19 19:09:05 -0300

    refactoring and moving ignore_pattern to Server module

--- apps/server/lib/server.ex
+++ apps/server/lib/server.ex
@@ -3,21 +3,57 @@ defmodule Server do
   alias Server.Sns
   alias Server.Events
 
-  def sqs(prefixes) do
+  def sqs(prefixes, opts \\ []) do
+    ignore_pattern = opts[:ignore_pattern]
+
     prefixes
-    |> Sqs.list()
-    |> Enum.map(&Server.Parser.url_to_arn/1)
-    |> Enum.map(fn arn ->
-      %{
-        arn: arn,
-        subscriptions: Sns.subscriptions_by_arn(arn),
-        rules: Events.rules_by_arn(arn)
-      }
-    end)
+    |> list_queues(ignore_pattern)
+    |> convert_url_to_arn()
+    |> append_subscriptions_and_rules()
   end
 
   def sns(prefixes) do
     prefixes
     |> Sns.list()
+  end
+
+  defp list_queues(prefixes, ignore_pattern) do
+    Enum.map(prefixes, fn prefix ->
+      queues = Sqs.list_queues(prefix)
+
+      queues =
+        case ignore_pattern do
+          nil -> queues
+          _ -> queues |> Enum.filter(&(!(&1 =~ ignore_pattern)))
+        end
+
+      %{
+        prefix: prefix,
+        queues: queues
+      }
+    end)
+  end
+
+  defp convert_url_to_arn(queues) do
+    Enum.map(queues, fn queue_map ->
+      %{queue_map | queues: queue_map.queues |> Enum.map(&Server.Parser.url_to_arn/1)}
+    end)
+  end
+
+  defp append_subscriptions_and_rules(queues) do
+    Enum.map(queues, fn queue_map ->
+      %{
+        queue_map
+        | queues:
+            queue_map.queues
+            |> Enum.map(fn queue ->
+              %{
+                arn: queue,
+                subscriptions: Sns.subscriptions_by_arn(queue),
+                rules: Events.rules_by_arn(queue)
+              }
+            end)
+      }
+    end)
   end
 end
--- apps/server/lib/server/sqs.ex
+++ apps/server/lib/server/sqs.ex
@@ -1,20 +1,11 @@
 defmodule Server.Sqs do
-  def list(prefixes, opts \\ []) do
-    ignore_pattern = opts[:ignore_pattern]
+  def list_queues(prefix) do
+    case ExAws.SQS.list_queues(queue_name_prefix: prefix) |> ExAws.request() do
+      {:ok, response} ->
+        response.body.queues
 
-    Enum.flat_map(prefixes, fn prefix ->
-      case ExAws.SQS.list_queues(queue_name_prefix: prefix) |> ExAws.request() do
-        {:ok, response} ->
-          response.body.queues
-
-          case ignore_pattern do
-            nil -> response.body.queues
-            _ -> response.body.queues |> Enum.filter(&(!(&1 =~ ignore_pattern)))
-          end
-
-        _ ->
-          []
-      end
-    end)
+      _ ->
+        []
+    end
   end
 end
